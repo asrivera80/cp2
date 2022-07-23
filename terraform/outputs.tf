65e418631ab30962ba63bccd0c8e858fc048cc3c
asrivera80 <104415467+asrivera80@users.noreply.github.com>
2022-07-23 16:38:55 +0200

    cambios finales terraform

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,12 +2,3 @@ output "resource_group_id" {
   value = azurerm_resource_group.rg-k8.id
 }
 
-output "public_ip_address_master" {
-  description = "Public IP Master"
-  value = azurerm_public_ip.pubmaster.ip_address
-}
-output "network_interface_master" {
-  description = "Private ip Master"
-  value       = azurerm_network_interface.NicMaster.private_ip_address
-}
-
